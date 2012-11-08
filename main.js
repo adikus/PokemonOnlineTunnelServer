5683fc19524b554522e6a6900b7c6f60135905c1
Andrej Hoos <andrej.hoos@gmail.com>
2012-11-08 08:47:49 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+var http = require('http');
+var net = require('net');
+var dataBuffer = [];
+var client;
+var fs = require('fs'),
+fstream = fs.createWriteStream("mains.data");
+
+client = net.connect({port: 5080, host:'188.165.249.120'},
+function() { //'connect' listener
+	console.log('connected to PO server\n');
+});
+
+client.on('data', function(data) {
+	dataBuffer.push(data);
+    //console.log('Data stored in buffer.\n');
+});
+
+client.on('end', function() {
+	console.log('Server disconnected.\n');
+});
+
+http.createServer(function (request, response) {
+	 
+    //console.log('request starting...');
+    
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+    var data = dataBuffer.shift();
+    if(data){
+        console.log('\nPO server --> Tunnel server');
+        //console.log(data.toString('utf8'));
+    }
+    response.end(data);    
+    
+    request.on('data', function(chunk) {		
+		console.log('\nTunnel server --> PO server');
+		//console.log(chunk.toString('utf8'));
+		client.write(chunk);
+		//fstream.write(chunk);
+    });
+    /*
+    request.on('end', function() {
+    	console.log('request ending...');
+    });*/
+     
+}).listen(process.env.PORT || 80);
+ 
+//Put a friendly message on the terminal of the server.
+console.log('Tunnel server running at port '+(process.env.PORT || 80));
